692dc33bbad5a1bb275219a5655788031242d3ae
edgars_kirkilevics <v-ekirkilevics@expediagroup.com>
2026-08-10 16:52:57 +0300

    Initialize Spring Boot project

---
+++ src/main/java/com/epam/aisupportcopilot/AiSupportCopilotApplication.java
@@ -0,0 +1,13 @@
+package com.epam.aisupportcopilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiSupportCopilotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiSupportCopilotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/epam/aisupportcopilot/AiSupportCopilotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.aisupportcopilot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiSupportCopilotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
